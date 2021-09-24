f51927c9a773599dab1f85521ad6692ca6562545
rqtrinh <rqtrinh@cpp.edu>
2021-09-23 19:44:01 -0700

    Initial commit for Restaurant Bill program with outline in comments

---
+++ restaurantBill.c
@@ -0,0 +1,39 @@
+/*Import necessary libraries
+
+/*Create main function with input parameters argc and argv
+So that we can accept input from command line*/
+
+/*Initialize a string array "meals" with possible meal names*/
+
+/*Initialize an int array "prices" that represents the price of meals
+in the matching index of "meals"*/
+
+/*Declare int variables for "mealCost", "taxAmount","tipAmount" &"totalBill" 
+Declare string variable for "mealName" */
+
+/*Declare int variables for "taxPercentage" & "tipPercentage" 
+which will be set to user input*/
+
+/*Declare int "index" which will be the index for the "meal" and "price" 
+in arrays "meals" & "prices"*/
+
+/*Declare time_t "t" for random number generator
+
+/*Init random number genereator*/
+
+/*Set "index" equal to random number 0-3 which we will use to select random meal*/
+
+/*Use index to set "mealName" and "mealCost" */
+
+/*Ask user for tip percentage
+
+/*Store input in "taxPercentage"*/
+
+/*Ask user for tip amount*/
+
+/*Store input in "tipPercentage"*/
+
+/*Calculate and store appropriate values to "taxAmount","tipAmount" & "totalBill"*/
+
+/*Print out meal cost, tax amount, tip amount, & total bill*/
+
